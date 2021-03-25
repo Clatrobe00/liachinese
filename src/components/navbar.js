e587273287820299d8defc8864ca9d7e546b8139
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-24 19:45:55 -0700

    install react-router-dom

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NavBar () {
     return(
+        <>
+            <div className='navbar'>
+                <div className='navbar-container'>
 
+                </div>
+            </div>
+        </>
     )
 }
 
