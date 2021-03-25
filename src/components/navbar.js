72454426df2890ecdec73ebb2b7d87931e9744ab
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-25 11:33:52 -0700

    add navlinks to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,19 +4,29 @@ import { Link } from 'react-router-dom';
 function NavBar () {
     const [click, setClick] =  useState(false);
 
-    const handleMenuClick = () => setClick(!click); 
+    const handleMenuClick = () => setClick(!click);
+    
+    const closeMenu = () => setClick(false);
     return(
         <>
                 <div className='navbar'>
                     <div className='navbarContainer'>
                         <Link to='/' className='navbarLogo'>
-                            Lia Liu Chinese
+                            <h1>Lia Liu Chinese</h1>
                         </Link>
                         <div className='menuIcon' onClick={handleMenuClick}>
                             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                         </div>
-                        <ul className='menuList'>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                             <li className='menuItem'>
+                                <Link to='/' className='navLink' onClick={closeMenu}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li className='menuItem'>
+                                <Link to='/videos' className='navLink' onClick={closeMenu}>
+                                    Videos
+                                </Link>
                             </li>
                         </ul>
                     </div>
