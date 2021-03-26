63eb763a6741b77955c726a93fef1ebf4fbda6d6
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-26 11:18:53 -0700

    comment out unused code.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,42 +1,48 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function NavBar () {
-    const [click, setClick] =  useState(false);
+// function NavBarPractice () {
+//     const [click, setClick] =  useState(false);
 
-    const handleMenuClick = () => setClick(!click);
+//     const handleMenuClick = () => setClick(!click);
     
-    const closeMenu = () => setClick(false);
-    return(
-        <>
-                <div className='navbar'>
-                    <div className='navbarContainer'>
-                        <Link to='/' className='navbarLogo'>
-                            <h1>Lia Liu Chinese</h1>
-                        </Link>
-                        <div className='menuIcon' onClick={handleMenuClick}>
-                            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                        </div>
-                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                            <li className='menuItem'>
-                                <Link to='/' className='navLink' onClick={closeMenu}>
-                                    Home
-                                </Link>
-                            </li>
-                            <li className='menuItem'>
-                                <Link to='/videos' className='navLink' onClick={closeMenu}>
-                                    Videos
-                                </Link>
-                            </li>
-                            <li className='menuItem'>
-                                <Link to='/sign-up' className='navLink-mobile' onClick={closeMenu}>
-                                    Sign Up!
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-        </>
+//     const closeMenu = () => setClick(false);
+//     return(
+//         <>
+//                 <div className='navbar'>
+//                     <div className='navbarContainer'>
+//                         <Link to='/' className='navbarLogo'>
+//                             <h1>Lia Liu Chinese</h1>
+//                         </Link>
+//                         <div className='menuIcon' onClick={handleMenuClick}>
+//                             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+//                         </div>
+//                         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+//                             <li className='menuItem'>
+//                                 <Link to='/' className='navLink' onClick={closeMenu}>
+//                                     Home
+//                                 </Link>
+//                             </li>
+//                             <li className='menuItem'>
+//                                 <Link to='/videos' className='navLink' onClick={closeMenu}>
+//                                     Videos
+//                                 </Link>
+//                             </li>
+//                             <li className='menuItem'>
+//                                 <Link to='/sign-up' className='navLink-mobile' onClick={closeMenu}>
+//                                     Sign Up!
+//                                 </Link>
+//                             </li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//         </>
+//     )
+// }
+
+function NavBar () {
+    return (
+        <div>Hello</div>
     )
 }
 
