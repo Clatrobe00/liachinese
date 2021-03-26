56bd4da454556374116da4c33fec1f08c2b269f1
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-26 11:20:14 -0700

    add bootstrap... again

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from './components/navbar';
