5d06756d05d9d5b6120ed5ee64c0f2d4d4da2bf0
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-26 14:01:50 -0700

    add first video to video page.

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,45 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Carousel } from 'react-bootstrap';
+import React from 'react';
+import ReactPlayer from 'react-player'
+
 
 
 function Videos () {
     return (
         <>
-            <Carousel fade>
-                <Carousel.Item>
-                <video width="320" height="240" controls>
-                <source src="../../public/videos/ac3217f8b8bb38155ef1087d7d9dbd24.MOV" type="video/mp4"/>
-                </video>
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Secondslide&bg=282c34"
-                    alt="Second slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src="holder.js/800x400?text=Third slide&bg=20232a"
-                    alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+           <ReactPlayer url="https://www.youtube.com/watch?v=Hgz3yizw_30&t=5s"/> 
         </>
     )
 };
