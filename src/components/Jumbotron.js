a0ae56fafae8502ec53d002812c4fc9cdc27be89
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-28 11:21:27 -0700

    add new banner image and style jumbotron.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap'
 import './Jumbotron.css'
+import image from '../assets/lanterns-banner.png'
 
 const JumbotronCustom = () => (
     <>    
-        <Jumbo fluid className='Jumbo'>
+        <Jumbo fluid className='Jumbo' style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', position: 'relative', alt: 'Chinese Lanterns Photo by mentatdgt from Pexels'}}>
             <div className='overlay'></div>
                 <Container>
-                    <h1>Welcome</h1>
-                    <p>The Funniest Chinese teacher you can find.</p>
+                    <h1 className='Jumbo-text'>Welcome</h1>
+                    <h2 className='Jumbo-text'>to Lia Liu Chinese</h2>
                 </Container>
 
         </Jumbo>
