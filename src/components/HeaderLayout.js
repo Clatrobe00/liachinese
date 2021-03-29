cce441f0af4945fe9f065ff2bb04c2d391ee00dc
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-29 11:26:12 -0700

    add headerLayout component

---
+++ src/components/HeaderLayout.js
@@ -0,0 +1,10 @@
+import Container from 'react-bootstrap/Container';
+
+const HeaderLayout = () => (
+    <Container>
+        <h1 className='Jumbo-text'>Welcome</h1>
+        <h2 className='Jumbo-text'>to Lia Liu Chinese</h2>
+    </Container>
+)
+
+export default HeaderLayout;
