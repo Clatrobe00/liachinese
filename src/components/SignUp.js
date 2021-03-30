96a4dbbcf2893b43b7087a057b8bc7dd5e9b0528
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-03-29 18:46:04 -0700

    add state functionality to signUp form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Button from './Button';
 
 function SignUp () {
+    const [signUp, setSignUp] = useState({
+        name: '',
+        email: '',
+        lessons: false,
+        materials: false,
+
+    });
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setSignUp({...signUp, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = () => {
+        console.log(signUp);
+    }
+
+    const handleCheckBox = (e) => {
+        setSignUp({...signUp, [e.target.name]: e.target.checked})
+    }
+
     return (
         <div>
             <Form>
@@ -13,7 +34,7 @@ function SignUp () {
                     Your Name
                     </Form.Label>
                     <Col sm="10">
-                    <Form.Control placeholder="Your name here" />
+                    <Form.Control name='name' value={signUp.name} onChange={handleChange} placeholder="Your name here" />
                     </Col>
                 </Form.Group>
 
@@ -22,7 +43,7 @@ function SignUp () {
                     Your Email
                     </Form.Label>
                     <Col sm="10">
-                    <Form.Control type="email" placeholder="your@email.here" />
+                    <Form.Control name='email' value={signUp.email} onChange={handleChange} type="email" placeholder="your@email.here" />
                     </Col>
                 </Form.Group>
                 <Form.Group>
@@ -31,6 +52,8 @@ function SignUp () {
                     {['Chinese Lessons', 'FREE Chinese Learning Materials'].map((i) => (
                         <div key={i} className="mb-3">
                         <Form.Check 
+                            name={i.split(' ')[i.split(' ').length - 1].toLowerCase()}
+                            onChange={handleCheckBox}
                             type='checkbox'
                             id={`checkbox-${i}`}
                             label={`${i}`}
@@ -38,7 +61,7 @@ function SignUp () {
                         </div>
                     ))}
                 </Form.Group>
-                <Button>
+                <Button onClick={handleSubmit}>
                     Submit
                 </Button>
             </Form>
